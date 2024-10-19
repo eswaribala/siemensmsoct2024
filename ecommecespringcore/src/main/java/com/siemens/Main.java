9dcc9174c5e95f43f9e4c4ea351cd5d620ee525d
Parameswari Bala <parameswaribala@gmail.com>
2024-10-19 12:07:26 +0530

    updated

--- ecommecespringcore/src/main/java/com/siemens/Main.java
+++ ecommecespringcore/src/main/java/com/siemens/Main.java
@@ -3,13 +3,16 @@ package com.siemens;
 import com.siemens.models.CreditCard;
 import com.siemens.models.UserAccount;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext=new
+        AbstractApplicationContext applicationContext=new
                 ClassPathXmlApplicationContext("ecommerce-config.xml");
+        applicationContext.registerShutdownHook();
+
         //Credit Card Instance
         //IOC
         //CreditCard creditCard= (CreditCard) applicationContext.getBean("creditCard");
@@ -19,6 +22,12 @@ public class Main {
         UserAccount userAccount= (UserAccount) applicationContext
                 .getBean("userAccount");
 
+        System.out.println(userAccount);
+
+        UserAccount userAccount1=(UserAccount) applicationContext
+                .getBean("userAccount");
+        userAccount1.setAccountNo(123);
+        //singleton
         System.out.println(userAccount);
 
 
--- ecommecespringcore/src/main/java/com/siemens/models/UserAccount.java
+++ ecommecespringcore/src/main/java/com/siemens/models/UserAccount.java
@@ -1,19 +1,29 @@
 package com.siemens.models;
 
+import jakarta.annotation.PreDestroy;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
+
 public class UserAccount {
     private long accountNo;
     private String name;
     @Autowired
     //dependency injection
     private CreditCard creditCard;
+
+    @PreDestroy
+    public void beforeDestroy(){
+
+        System.out.println("It's about to dispose the bean, close all the files/connections....");
+
+    }
 }
