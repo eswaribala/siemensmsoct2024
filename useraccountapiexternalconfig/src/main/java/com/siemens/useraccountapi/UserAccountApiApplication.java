2cceefe80ae1899eebe016b1b21bf4365fac7299
Parameswari Bala <parameswaribala@gmail.com>
2024-10-27 21:43:46 +0530

    updated

--- useraccountapiexternalconfig/src/main/java/com/siemens/useraccountapi/UserAccountApiApplication.java
+++ useraccountapiexternalconfig/src/main/java/com/siemens/useraccountapi/UserAccountApiApplication.java
@@ -3,6 +3,7 @@ package com.siemens.useraccountapi;
 import com.siemens.useraccountapi.configurations.VaultConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
