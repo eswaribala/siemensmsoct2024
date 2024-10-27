0df3101c29f070818463771f3f26d438c8a5db7f
Parameswari Bala <parameswaribala@gmail.com>
2024-10-27 14:55:59 +0530

    updaed

---
+++ useraccountapiexternalconfig/src/main/java/com/siemens/useraccountapi/configurations/DbConfiguration.java
@@ -0,0 +1,51 @@
+package com.siemens.useraccountapi.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@EnableConfigurationProperties(VaultConfiguration.class)
+public class DbConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    private DataSourceBuilder dataSourceBuilder;
+  //  private final VaultConfiguration vaultConfiguration;
+
+   /*
+    public DbConfiguration (VaultConfiguration vaultConfig)
+    {
+
+        this.vaultConfiguration=vaultConfig;
+    }
+
+*/
+    @Bean
+    public DataSource getDataSource()
+    {
+        System.out.println(dbUrl);
+        System.out.println(driver);
+
+        System.out.println("Entering Given Env.....");
+      //  System.out.println("User Name..."+vaultConfiguration.getMysqlusername());
+       // System.out.println("Password..."+vaultConfiguration.getPassword());
+        //System.out.println("User Name..."+vaultConfiguration.getUsername1());
+        //System.out.println("Password..."+vaultConfiguration.getPassword1());
+        dataSourceBuilder=DataSourceBuilder.create();
+        dataSourceBuilder.url(dbUrl);
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("password");
+        dataSourceBuilder.driverClassName(driver);
+        return dataSourceBuilder.build();
+
+    }
+
+}
---
+++ useraccountapiexternalconfig/src/main/java/com/siemens/useraccountapi/configurations/VaultConfiguration.java
@@ -0,0 +1,13 @@
+package com.siemens.useraccountapi.configurations;
+/*
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@Data
+@ConfigurationProperties
+public class VaultConfiguration {
+    private String mysqlusername;
+    private String password;
+}
+*/
